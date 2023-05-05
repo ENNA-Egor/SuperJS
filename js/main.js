7e3bd1a117f29c0f0a8266031d0da1770f45d0eb
ENNA-Egor <e.yudakov@gmail.com>
2023-05-05 15:18:18 +0300

    Незначительные изменения

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 for (let key in COLOR_PALLETE){
     let row = document.createElement('tr')
-    row.innerHTML= `<th colspan="10">${key}</th>`
+    row.innerHTML= `<th colspan="5">${key}</th>`
     document.querySelector('table').append(row)
     for(let i=0; i<COLOR_PALLETE[key].length; i++ ){
         let row = document.createElement('tr')
         row.innerHTML =`
-            <td colspan="3" class="pod">${COLOR_PALLETE[key][i][0]}</td>
+            <td colspan="4" class="pod">${COLOR_PALLETE[key][i][0]}</td>
             `
             document.querySelector('table').append(row)
             const table = document.querySelector('table');
             let colCount = 0;
             let currentRow = document.createElement('tr');
             table.append(currentRow);
-            
+
             for (let n = 1; n < COLOR_PALLETE[key][i].length; n++) {
               let newCol = document.createElement('td');
               let bgColor = ('#' + COLOR_PALLETE[key][i][n]);
