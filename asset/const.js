382295f4177b978e645d416bc05ad358ebc7cdc0
ENNA-Egor <e.yudakov@gmail.com>
2023-05-24 11:34:38 +0300

    Ред файл сохранённых палитр

--- asset/const.js
+++ asset/const.js
@@ -3,13 +3,13 @@ const COLOR_PALLETE ={
     'Default': {
         'Бледный':['004358', '1f8a70', 'bedb39', 'ffff1a', '7f7fff'],
         'Яркий': ['7f44eb', '628f2e', '92fce3', 'ff00ff'],
-        'Нейтральный': ['2f6b6b', '2f5151','104646', '6cb7b7', '80b7b7', '7f7fff', 'c1faec', 'ba8db6', '7f44eb','004358', 'ffff1a','1f8a70', 'bedb39', 'ffff1a', '7f7fff', 'c1faec']
+        'Нейтральный': ['2f6b6b', '2f5151','104646', '6cb7b7', '80b7b7', '7f7fff', 'c1faec', 'ba8db6', '7f44eb','004358', 'ffff1a','1f8a70', 'bedb39', 'ffff1a', '7f7fff']
     },
     'Custom': {
         'Glow': ['9a9afa', 'c9b1f4', '91a679'],
         'Золотой': ['5b82b3', '86cab9', 'd5e390', 'fafa98', 'c1faec'],
         'Любимый':['004358', '1f8a70', 'bedb39', 'ffff1a', '7f7fff'],
-        'Новый 6': ['d5e390', '628f2e', '14dcdc','fafa98','1f8a70', 'bedb39', 'ffff1a', '7f7fff', 'c1faec', 'd5e390', '628f2e', '14dcdc','fafa98','1f8a70', 'bedb39', 'ffff1a', '7f7fff'],
+        'Новый 6': ['d5e390', '628f2e', '14dcdc','fafa98','1f8a70', 'bedb39', 'ffff1a', '7f7fff', 'c1faec', 'd5e390', '628f2e', '14dcdc','fafa98','1f8a70', 'bedb39'],
         'Добавленный':['004358', '1f8a70','fafa98','1f8a70']
     },
 }
